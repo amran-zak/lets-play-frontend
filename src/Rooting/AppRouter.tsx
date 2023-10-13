9034427f77c096ee568abd118e3f48884f3c614f
perrineosw <oswald.perrinedu40@gmail.com>
2023-10-13 13:40:05 +0200

    login-page: first commit

--- src/Rooting/AppRouter.tsx
+++ src/Rooting/AppRouter.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "../Components/NavBar/Header";
+import Login from "../Components/Authentification/Login";
 
 
 export default function AppRouter(): JSX.Element {
@@ -9,10 +10,10 @@ export default function AppRouter(): JSX.Element {
       <div className="app-container">
         <div className="app-body">
           <Header />
-          {/*<Routes>*/}
+          <Routes>
           {/*  <Route path="/" element={<Home/>} />*/}
           {/*  <Route path="/details" element={<Details />} />*/}
-          {/*  <Route path="/connexion" element={<Login/>} />*/}
+            <Route path="/connexion" element={<Login/>} />
           {/*  <Route path="/inscription" element={<SignUp/>} />*/}
           {/*  <Route path="/nouveau_mot_de_passe" element={<New_password/>} />*/}
           {/*  <Route path="/profile" element={<UserProfile/>} />*/}
@@ -20,7 +21,7 @@ export default function AppRouter(): JSX.Element {
           {/*  <Route path="/ajouter_annonce" element={<Add_Annonces/>} />*/}
           {/*  /!*<Route path='/forbiddenAccess' element={<LoginPage/>} />*!/*/}
           {/*  <Route path="/*" element={<NotFoundPage />} />*/}
-          {/*</Routes>*/}
+          </Routes>
         </div>
       </div>
     </BrowserRouter>
