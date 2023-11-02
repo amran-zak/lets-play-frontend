e567b8cb6f32b8422adc573da5915eb0975d52cd
perrineosw <oswald.perrinedu40@gmail.com>
2023-11-02 10:44:31 +0100

    change front

--- src/Components/Organizer/CreateAnnounce.tsx
+++ src/Components/Organizer/CreateAnnounce.tsx
@@ -10,7 +10,7 @@ import {
   Container,
   Paper, debounce
 } from '@mui/material'
-import Map, { Marker, NavigationControl } from 'react-map-gl'
+import Map, {Marker, NavigationControl} from 'react-map-gl'
 import 'mapbox-gl/dist/mapbox-gl.css'
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined'
 import {useForm} from 'react-hook-form'
@@ -19,6 +19,9 @@ import {yupResolver} from '@hookform/resolvers/yup'
 import background from '../Images/chaussure.jpeg'
 import AnnounceData from '../../Types/Announce.types'
 import {useCallback, useState} from 'react'
+import UserData from '../../Types/User.types'
+import Authentification from '../../Services/Authentification'
+import SportsList from '../SportsList'
 
 interface CitySuggestion {
   id: number
@@ -30,17 +33,20 @@ interface CitySuggestion {
 
 export default function CreateAnnounce() {
   const validationSchema = Yup.object().shape({
-    sport: Yup.string().required('Le choix du sport est requis'),
+    sport: Yup.string(),
     numberOfPeopleMax: Yup.number()
       .typeError('Le nombre de participants maximal doit être un nombre')
       .required('Le nombre de participants maximum que peut accueillir votre évènement'),
-    date: Yup.date().required('La date de l\'évènement est requise'),
-    startTime: Yup.date().required('L\'heure de début de l\'évènement est requise'),
-    endTime: Yup.date()
-      .required('L\'heure de fin de l\'évènement est requise')
-      .when('startTime', (startTime, schema) => {
-        return schema.min(startTime, 'L\'heure de fin doit être postérieure à l\'heure de début')
-      }),
+    date: Yup.string()
+      .transform((value, originalValue) => {
+        // Assurez-vous que la date est sous le format "AAAA-MM-JJ" (année-mois-jour)
+        if (originalValue && !originalValue.match(/^\d{4}-\d{2}-\d{2}$/)) {
+          return null
+        }
+        return value
+      }).required('La date de l\'évènement est requise'),
+    startTime: Yup.string().required('L\'heure de début de l\'évènement est requise'),
+    endTime: Yup.string().required('L\'heure de fin de l\'évènement est requise'),
     address: Yup.string().required('L\'adresse est requise'),
     city: Yup.string(),
     ageMin: Yup.number()
@@ -101,6 +107,7 @@ export default function CreateAnnounce() {
   }
 
   const [adresseInput, setAdresseInput] = useState('')
+  const [cityInput, setCityInput] = useState('')
   const [latitudeInput, setLatitudeInput] = useState(48.866667)
   const [longitudeInput, setLongitudeInput] = useState(2.333333)
   const debouncedFetchCitySuggestions = useCallback(
@@ -115,185 +122,223 @@ export default function CreateAnnounce() {
 
   const handleCitySuggestionClick = (suggestion: CitySuggestion) => {
     setAdresseInput(suggestion.label)
+    setCityInput(suggestion.city)
     setLatitudeInput(suggestion.latitude)
     setLongitudeInput(suggestion.longitude)
     setCitySuggestions([])
   }
 
+  const [selectedSport, setSelectedSport] = useState<string>('')
+
+  const handleSportChange = (selectedSport: string) => {
+    console.log(selectedSport)
+    setSelectedSport(selectedSport)
+  }
+
+  const onSubmit = (data: AnnounceData) => {
+    const addData: AnnounceData = {
+      sport: selectedSport,
+      numberOfPeopleMax: data.numberOfPeopleMax,
+      date: data.date,
+      startTime: data.startTime,
+      endTime: data.endTime,
+      address: adresseInput,
+      city: cityInput,
+      ageMin: data.ageMin,
+      ageMax: data.ageMax,
+      price: data.price
+    }
+    console.log('user : ', addData)
+  //   Authentification.signUp(signUpData)
+  //     .then((response: any) => {
+  //       console.log(response)
+  //       if (response.data.token) {
+  //         localStorage.setItem('token', response.data.token)
+  //       }
+  //     })
+  //     .catch((error: Error) => {
+  //       console.error(error)
+  //     })
+  }
+
   const token = 'pk.eyJ1IjoiZ2lzZmVlZGJhY2siLCJhIjoiY2l2eDJndmtjMDFkeTJvcHM4YTNheXZtNyJ9.-HNJNch_WwLIAifPgzW2Ig'
 
   return (
-    <Container component="main"
+    <Grid
+      container
+      component='main'
       sx={{
-        minWidth: '100%',
-        background: `url(${background})`,
-        backgroundSize: 'cover',
-        minHeight: '100vh'
+        overflowY: 'scroll',
+        height: '100vh'
       }}
     >
-      <CssBaseline />
-      <Box maxWidth="md" component={Paper}
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          mx: 'auto',
-          p: 5,
-          justifyContent: 'center',
-          minHeight: '100vh'
-        }}
-      >
-        <Avatar sx={{
-          mx: 'auto',
-          bgcolor: 'secondary.main'
-        }}>
-          <LockOutlinedIcon/>
-        </Avatar>
-        <Typography component='h1' variant='h5' sx={{ marginBottom: 5}}>
-          Créer un évènement
-        </Typography>
-        <form>
-          <Grid container spacing={2}>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                autoComplete='given-name'
-                required
-                fullWidth
-                id='sport'
-                label='Sport'
-                autoFocus
-                {...register('sport')}
-                error={!!errors.sport}
-                helperText={errors.sport?.message}
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                required
-                fullWidth
-                id='price'
-                label='Prix'
-                autoComplete='price'
-                {...register('price')}
-                error={!!errors.price}
-                helperText={errors.price?.message}
-              />
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <TextField
-                required
-                fullWidth
-                id='date'
-                label='Date'
-                autoComplete='date'
-                {...register('date')}
-                error={!!errors.date}
-                helperText={errors.date?.message}
-              />
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <TextField
-                required
-                fullWidth
-                id='startTime'
-                label='Horaire début'
-                autoComplete='startTime'
-                {...register('startTime')}
-                error={!!errors.startTime}
-                helperText={errors.startTime?.message}
-              />
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <TextField
-                required
-                fullWidth
-                id='endTime'
-                label='Horaire de fin'
-                autoComplete='endTime'
-                {...register('endTime')}
-                error={!!errors.endTime}
-                helperText={errors.endTime?.message}
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                required
-                fullWidth
-                id='address'
-                label='Lieux'
-                value={adresseInput}
-                {...register('address')}
-                error={!!errors.address}
-                helperText={errors.address?.message}
-                onChange={handleCityInputChange}
-              />
-              {citySuggestions.length > 0 && (
-                <ul>
-                  {citySuggestions.map((suggestion, index) => (
-                    <li key={index} onClick={() => handleCitySuggestionClick(suggestion)} style={{ cursor: 'pointer' }}>{suggestion.label}</li>
-                  ))}
-                </ul>
-              )}
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <Map
-                initialViewState={{
-                  longitude: longitudeInput,
-                  latitude: latitudeInput,
-                  zoom: 14
-                }}
-                mapboxAccessToken={token}
-                style={{ width: '100%', height: 400 }}
-                mapStyle='https://basemaps.cartocdn.com/gl/positron-gl-style/style.json'
-              >
-                <NavigationControl />
-                <Marker longitude={longitudeInput} latitude={latitudeInput} style={{ color: 'white' }}/>
-              </Map>
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                required
-                fullWidth
-                id='numberOfPeopleMax'
-                label='Nombre maximal de participants'
-                autoComplete='numberOfPeopleMax'
-                {...register('numberOfPeopleMax')}
-                error={!!errors.numberOfPeopleMax}
-                helperText={errors.numberOfPeopleMax?.message}
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                required
-                fullWidth
-                id='ageMin'
-                label='Âge minimal requis'
-                autoComplete='ageMin'
-                {...register('ageMin')}
-                error={!!errors.ageMin}
-                helperText={errors.ageMin?.message}
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                required
-                fullWidth
-                id='ageMax'
-                label='Âge maximal'
-                autoComplete='ageMax'
-                {...register('ageMax')}
-                error={!!errors.ageMax}
-                helperText={errors.ageMax?.message}
-              />
-            </Grid>
-          </Grid>
-          <Button type='submit' fullWidth variant='contained' sx={{
-            marginTop: 3,
-            marginBottom: 2
+      <CssBaseline/>
+      <Grid item xs={12} sm={6}>
+        <Map
+          initialViewState={{
+            longitude: longitudeInput,
+            latitude: latitudeInput,
+            zoom: 5
+          }}
+          mapboxAccessToken={token}
+          style={{
+            width: '100%',
+            height: '80%',
+            top: '10%'
+          }}
+          mapStyle='https://basemaps.cartocdn.com/gl/positron-gl-style/style.json'
+        >
+          <NavigationControl/>
+          <Marker longitude={longitudeInput} latitude={latitudeInput} style={{color: 'white'}}/>
+        </Map>
+      </Grid>
+      <Grid item xs={12} sm={6} component={Paper} elevation={6} square sx={{
+        my: 'auto',
+        boxShadow: 'none'
+      }}>
+        <Box
+          sx={{
+            my: 8,
+            mx: 4,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center'
+          }}
+        >
+          <Avatar sx={{
+            mx: 'auto',
+            bgcolor: 'secondary.main'
           }}>
-            Ajouter
-          </Button>
-        </form>
-      </Box>
-    </Container>
+            <LockOutlinedIcon/>
+          </Avatar>
+          <Typography component='h1' variant='h5' sx={{marginBottom: 5}}>
+            Créer un évènement
+          </Typography>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <SportsList onSportChange={handleSportChange} />
+                {selectedSport === '' && <Typography variant="caption" display="block" gutterBottom>Ce champ est requis</Typography>}
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  id='price'
+                  label='Prix'
+                  autoComplete='price'
+                  {...register('price')}
+                  error={!!errors.price}
+                  helperText={errors.price?.message}
+                />
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                <TextField
+                  required
+                  fullWidth
+                  id='date'
+                  label='Date'
+                  autoComplete='date'
+                  type='date'
+                  InputLabelProps={{
+                    shrink: true
+                  }}
+                  {...register('date')}
+                  error={!!errors.date}
+                  helperText={errors.date?.message}
+                />
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                <TextField
+                  required
+                  fullWidth
+                  id='startTime'
+                  label='Horaire début'
+                  autoComplete='startTime'
+                  {...register('startTime')}
+                  error={!!errors.startTime}
+                  helperText={errors.startTime?.message}
+                />
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                <TextField
+                  required
+                  fullWidth
+                  id='endTime'
+                  label='Horaire de fin'
+                  autoComplete='endTime'
+                  {...register('endTime')}
+                  error={!!errors.endTime}
+                  helperText={errors.endTime?.message}
+                />
+              </Grid>
+              <Grid item xs={12} sm={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id='address'
+                  label='Lieux'
+                  value={adresseInput}
+                  {...register('address')}
+                  error={!!errors.address}
+                  helperText={errors.address?.message}
+                  onChange={handleCityInputChange}
+                />
+                {citySuggestions.length > 0 && (
+                  <ul>
+                    {citySuggestions.map((suggestion, index) => (
+                      <li key={index} onClick={() => handleCitySuggestionClick(suggestion)}
+                        style={{cursor: 'pointer'}}>{suggestion.label}</li>
+                    ))}
+                  </ul>
+                )}
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  id='numberOfPeopleMax'
+                  label='Nombre maximal de participants'
+                  autoComplete='numberOfPeopleMax'
+                  {...register('numberOfPeopleMax')}
+                  error={!!errors.numberOfPeopleMax}
+                  helperText={errors.numberOfPeopleMax?.message}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  id='ageMin'
+                  label='Âge minimal requis'
+                  autoComplete='ageMin'
+                  {...register('ageMin')}
+                  error={!!errors.ageMin}
+                  helperText={errors.ageMin?.message}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  id='ageMax'
+                  label='Âge maximal'
+                  autoComplete='ageMax'
+                  {...register('ageMax')}
+                  error={!!errors.ageMax}
+                  helperText={errors.ageMax?.message}
+                />
+              </Grid>
+            </Grid>
+            <Button type='submit' fullWidth variant='contained' sx={{
+              marginTop: 3,
+              marginBottom: 2
+            }}>
+              Ajouter
+            </Button>
+          </form>
+        </Box>
+      </Grid>
+    </Grid>
   )
 }
--- src/Types/Announce.types.tsx
+++ src/Types/Announce.types.tsx
@@ -2,11 +2,11 @@ import module from 'module'
 import Timestamp = module
 
 export default interface AnnounceData {
-  sport: string
+  sport?: string
   numberOfPeopleMax: number
-  date: Date
-  startTime: Date
-  endTime: Date
+  date: string
+  startTime: string
+  endTime: string
   address: string
   city?: string | undefined
   ageMin: number
