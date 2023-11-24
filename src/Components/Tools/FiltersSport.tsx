32144c02e6d0ec9ab651d6251b198d7b9a533eac
perrineosw <oswald.perrinedu40@gmail.com>
2023-11-24 17:28:26 +0100

    Update FiltersSport.tsx

--- src/Components/Tools/FiltersSport.tsx
+++ src/Components/Tools/FiltersSport.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useCallback } from 'react'
 import {
-  Button, FormControl, TextField, List, ListItem, ListItemText, Drawer, Divider, Grid, debounce
+  Button, FormControl, TextField, List, ListItem, ListItemText, Drawer, Divider, Grid, debounce, FormControlLabel,
+  Checkbox
 } from '@mui/material'
 import { Close, FilterAlt, FilterAltOff } from '@mui/icons-material'
 import publicService from '../../Services/Public'
@@ -29,7 +30,11 @@ const FilterComponent: React.FC<FilterProps> = ({sportsList, setSportsList}) => 
   const [searchSport, setSearchSport] = React.useState<string>('Football')
   const [isFiltered, setIsFiltered] = React.useState<boolean>(false)
   const [citySuggestions, setCitySuggestions] = useState<CitySuggestion[]>([])
-  const [searchDateStart, setSearchDateStart] = React.useState<Date | string>('')
+  const [searchSimpleDate, setSearchSimpleDate] = React.useState<Date | string>('')
+  const [checkedSimpleDate, setCheckedSimpleDate] = React.useState<boolean>(false)
+  const [searchBetweenStartDate, setSearchBetweenStartDate] = React.useState<Date | string>('')
+  const [searchBetweenEndDate, setSearchBetweenEndDate] = React.useState<Date | string>('')
+  const [checkedBetweenDate, setCheckedBetweenDate] = React.useState<boolean>(false)
 
   const handleCitySuggestionClick = (suggestion: CitySuggestion) => {
     setSearchCity(suggestion.city)
@@ -77,8 +82,29 @@ const FilterComponent: React.FC<FilterProps> = ({sportsList, setSportsList}) => 
     setSearchSport(sport)
   }
 
-  const handleSearchDateStart = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchDateStart(event.target.value)
+  const handleSearchSimpleDate = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchSimpleDate(event.target.value)
+  }
+
+  const handleSearchBetweenStartDate = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchBetweenStartDate(event.target.value)
+  }
+
+  const handleSearchBetweenEndDate = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchBetweenEndDate(event.target.value)
+  }
+
+  const handleChangeSimpleDate = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCheckedSimpleDate(event.target.checked)
+    setCheckedBetweenDate(false)
+    setSearchBetweenStartDate('')
+    setSearchBetweenEndDate('')
+  }
+
+  const handleChangeBetweenDate = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCheckedBetweenDate(event.target.checked)
+    setCheckedSimpleDate(false)
+    setSearchSimpleDate('')
   }
 
   const handleFilter = () => {
@@ -89,28 +115,35 @@ const FilterComponent: React.FC<FilterProps> = ({sportsList, setSportsList}) => 
       return property && typeof property === 'string' &&
         property.trim().toUpperCase().includes(searchTerm.trim().toUpperCase())
     }
+    const filterFunctionBetweenDates = (property: any, searchTerm1: string, searchTerm2: string) => {
+      if (!property || typeof property !== 'string') {
+          return false
+      }
+      const propertyDate = new Date(property)
+      if (isNaN(propertyDate.getTime())) {
+          return false
+      }
+      return propertyDate >= new Date(searchTerm1) && propertyDate <= new Date(searchTerm2)
+    }
     let filteredList
     switch (true) {
-      case searchCity !== '' && searchSport !== '' && searchDateStart !== '':
-        filteredList = sportsList.filter((element) => filterFunction(element.city, searchCity) && filterFunction(element.sport, searchSport) && filterFunction(element.date, searchDateStart.toString()))
+      case searchCity !== '' && searchSport !== '' && searchSimpleDate !== '':
+        filteredList = sportsList.filter((element) => filterFunction(element.city, searchCity) && filterFunction(element.sport, searchSport) && filterFunction(element.date, searchSimpleDate.toString()))
+        break
+      case searchCity !== '' && searchSport !== '' && searchBetweenStartDate !== '' && searchBetweenEndDate !== '':
+        filteredList = sportsList.filter((element) => filterFunction(element.city, searchCity) && filterFunction(element.sport, searchSport) && filterFunctionBetweenDates(element.date, searchBetweenStartDate.toString(), searchBetweenEndDate.toString()))
         break
       case searchCity !== '' && searchSport !== '':
         filteredList = sportsList.filter((element) => filterFunction(element.city, searchCity) && filterFunction(element.sport, searchSport))
         break
-      case searchCity !== '' && searchDateStart !== '':
-        filteredList = sportsList.filter((element) => filterFunction(element.city, searchCity) && filterFunction(element.date, searchDateStart.toString()))
+      case searchSimpleDate !== '' && searchSport !== '':
+        filteredList = sportsList.filter((element) => filterFunction(element.date, searchSimpleDate.toString()) && filterFunction(element.sport, searchSport))
         break
-      case searchDateStart !== '' && searchSport !== '':
-        filteredList = sportsList.filter((element) => filterFunction(element.date, searchDateStart.toString()) && filterFunction(element.sport, searchSport))
-        break
-      case searchCity !== '':
-        filteredList = sportsList.filter((element) => filterFunction(element.city, searchCity))
+      case searchBetweenStartDate !== '' && searchBetweenEndDate !== '' && searchSport !== '':
+        filteredList = sportsList.filter((element) => filterFunctionBetweenDates(element.date, searchBetweenStartDate.toString(), searchBetweenEndDate.toString()) && filterFunction(element.sport, searchSport))
         break
       case searchSport !== '':
         filteredList = sportsList.filter((element) => filterFunction(element.sport, searchSport))
-        break
-      case searchDateStart !== '':
-        filteredList = sportsList.filter((element) => filterFunction(element.date, searchDateStart.toString()))
         break
       default:
       filteredList = sportsList
@@ -133,7 +166,11 @@ const FilterComponent: React.FC<FilterProps> = ({sportsList, setSportsList}) => 
     setIsFiltered(false)
     setSearchCity('')
     setSearchSport('')
-    setSearchDateStart('')
+    setCheckedSimpleDate(false)
+    setSearchSimpleDate('')
+    setCheckedBetweenDate(false)
+    setSearchBetweenStartDate('')
+    setSearchBetweenEndDate('')
   }
 
   return (
@@ -156,12 +193,7 @@ const FilterComponent: React.FC<FilterProps> = ({sportsList, setSportsList}) => 
           >
             <List>
               <Grid container>
-                <Grid item md={9} xs={12}>
-                  <ListItem>
-                    <ListItemText primary="Filtrer les annonces" />
-                  </ListItem>
-                </Grid>
-                <Grid item md={3} xs={12} textAlign="end">
+                <Grid item md={12} xs={12} textAlign="end">
                   <Button
                     onClick={toggleMenu}
                     sx={{ color: 'red' }}
@@ -170,9 +202,8 @@ const FilterComponent: React.FC<FilterProps> = ({sportsList, setSportsList}) => 
                   </Button>
                 </Grid>
               </Grid>
-              {/* Ajoutez ici d'autres éléments de menu si nécessaire */}
               <ListItem>
-                <FormControl sx={{ my: 1, mt: 5, textAlign: 'center' }} size="small">
+                <FormControl sx={{ my: 2, textAlign: 'center' }}>
                   <TextField
                     fullWidth
                     label='Rechercher par nom de ville'
@@ -199,27 +230,74 @@ const FilterComponent: React.FC<FilterProps> = ({sportsList, setSportsList}) => 
                   <br />
                   <SportsList onSportChange={handleSearchSport} defaultValue={searchSport} disabled={isFiltered} />
                   <br />
-                  <Grid container>
-                    <Grid item xs={12} sm={4}>
-                      <TextField
-                        label='Date'
-                        autoComplete='searchDateStart'
-                        type='date'
-                        InputLabelProps={{
-                          shrink: true
-                        }}
-                        value={searchDateStart}
-                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleSearchDateStart(e)}
-                        disabled={isFiltered}
+                  <FormControlLabel
+                    label="Date précise"
+                    control={
+                      <Checkbox
+                        checked={checkedSimpleDate}
+                        onChange={handleChangeSimpleDate}
                       />
+                    }
+                  />
+                  <FormControlLabel
+                    label="Intervalle de dates"
+                    control={
+                      <Checkbox
+                        checked={checkedBetweenDate}
+                        onChange={handleChangeBetweenDate}
+                      />
+                    }
+                  />
+                  <br />
+                  {checkedSimpleDate &&
+                    <TextField
+                      label='Date'
+                      autoComplete='searchDateStart'
+                      type='date'
+                      InputLabelProps={{
+                        shrink: true
+                      }}
+                      value={searchSimpleDate}
+                      onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleSearchSimpleDate(e)}
+                      disabled={isFiltered}
+                    />
+                  }
+                  {checkedBetweenDate &&
+                    <Grid container>
+                      <Grid item md={6}>
+                        <TextField
+                          label='Début'
+                          autoComplete='searchBetweenStartDate'
+                          type='date'
+                          InputLabelProps={{
+                            shrink: true
+                          }}
+                          value={searchBetweenStartDate}
+                          onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleSearchBetweenStartDate(e)}
+                          disabled={isFiltered}
+                        />
+                      </Grid>
+                      <Grid item md={6}>
+                        <TextField
+                          label='Fin'
+                          autoComplete='searchBetweenEndDate'
+                          type='date'
+                          InputLabelProps={{
+                            shrink: true
+                          }}
+                          value={searchBetweenEndDate}
+                          onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleSearchBetweenEndDate(e)}
+                          disabled={isFiltered}
+                        />
+                      </Grid>
                     </Grid>
-                  </Grid>
+                  }
                   {!isFiltered && (
                     <Button
                       variant="contained"
                       sx={{ marginTop: '10px', bgcolor: 'secondary.main' }}
                       onClick={() => { handleFilter(); toggleMenu() }}
-                      disabled={searchCity === '' && searchSport === '' && searchDateStart === ''}
+                      disabled={(checkedSimpleDate && searchSimpleDate === '') || (checkedBetweenDate && searchBetweenStartDate === '' && searchBetweenEndDate === '')}
                     >
                       Valider
                     </Button>
